350b6b0a82716458689b3a694f2469763380607e
culturewide <eatfanbrother@gmail.com>
2025-04-02 19:27:03 +0800

    first commit

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,82 @@
+public class ArrayDeque<T> {
+    public int size;
+    public int frontPtr;
+    public int rearPtr;
+    public T[] items;
+    public int capacity;
+    public ArrayDeque () {
+        items = (T[]) new Object[8];
+        size=0;
+        frontPtr = 0;
+        rearPtr = 1;
+        capacity=8;
+    }
+    public void update(){
+        if(size==capacity){
+            resize(capacity*2);
+        }else if(size>0&&capacity>=16&&size*4<capacity){
+            resize(Math.max(capacity/2,16));
+        }
+    }
+    //循环需要用到+size/size的方法
+    public void resize(int newcapacity){
+        T[] newItems = (T[]) new Object[newcapacity];
+        for(int i=0; i<size; i++){
+            newItems[i]=items[(frontPtr+i+1)%size];
+        }
+        items = newItems;
+        frontPtr = 0;
+        rearPtr =size;
+        capacity=newcapacity;
+    }
+    public void addFirst(T num) {
+        update();
+        items[frontPtr]=num;
+        size++;
+        frontPtr = (frontPtr-1+capacity)%capacity;
+    }
+    public boolean isEmpty() {
+        return size==0;
+    }
+    public int size() {
+        return size;
+    }
+    public void addLast(T item){
+        update();
+        items[rearPtr]=item;
+        size++;
+        rearPtr = (rearPtr+1+capacity)%capacity;
+    }
+    public T removeFirst() {
+        if(size==0){
+            return null;
+        }
+        T x=items[frontPtr+1];
+        frontPtr = (frontPtr+1+capacity)%capacity;
+        size--;
+        update();
+        return x;
+    }
+    public T removeLast() {
+        if(size==0){
+            return null;
+        }
+        T x=items[rearPtr-1];
+        rearPtr = (rearPtr-1+capacity)%capacity;
+        size--;
+        update();
+        return x;
+    }
+    public T get(int index) {
+        if(index>=size)
+            return null;
+        return items[(frontPtr+index+1)%capacity];
+
+    }
+    public void printDeque() {
+        for(int i=0; i<size; i++) {
+            System.out.print(items[(i+frontPtr+1)%capacity]+" ");
+        }
+
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,118 @@
+public class LinkedListDeque<T> {
+    public int size=0;
+    private class IntNode {
+        public T first;
+        public IntNode next;
+        public IntNode prev;
+        /* public IntNode() {
+             first = null;
+             next = null;
+         }*/
+        //在Java中，类的成员变量可以在类中直接声明和初始化，
+        // 但执行语句（如赋值操作）必须放在方法、构造函数或初始化块中。
+        public IntNode(T item, IntNode p, IntNode n) {
+            first=item;
+            next=p;
+            prev=n;
+        }
+    }
+    public IntNode sentinel ;
+    //构造函数不可以带void
+    public  LinkedListDeque(){
+        sentinel=new IntNode(null, null, null);
+        sentinel.next=sentinel;
+        sentinel.prev=sentinel;
+        size=0;
+
+    }
+
+
+    public void addFirst(T t) {
+        IntNode s=new IntNode(t,sentinel.next,sentinel);
+        sentinel.next.prev=s;
+        sentinel.next = s;
+        size+=1;
+    }
+    public void addLast(T t) {
+        IntNode s=new IntNode(t,sentinel,sentinel.prev);
+        sentinel.prev.next=s;
+        sentinel.prev = s;
+        size+=1;
+    }
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    public int size(){
+        return size;
+    }
+    public void printDeque() {
+        IntNode p = sentinel;
+        while(p.next!=sentinel){
+            p=p.next;
+            System.out.print(p.first+" ");
+        }
+    }
+    public T    removeFirst() {
+        if(sentinel==sentinel.next) {
+            return null;
+        }else {T x = sentinel.next.first;
+            sentinel.next = sentinel.next.next;
+            sentinel.next.prev = sentinel;
+            size-=1;
+            return x;
+        }
+    }
+    IntNode q = sentinel;
+    public static int aNum=0;
+    public T getRecursive(int index){
+        if(size<=index){
+            return null;
+        }
+        if(aNum!=index) {
+            q=q.next;
+            aNum++;
+            return getRecursive(index);
+        }else {
+            aNum=0;
+            return q.next.first;
+
+        }
+
+    }
+    public T    removeLast() {
+        if(sentinel==sentinel.prev) {
+            return null;
+        }else {T x = sentinel.prev.first;
+            sentinel.prev.prev.next = sentinel;
+            sentinel.prev=sentinel.prev.prev;
+            size-=1;
+            return x;}
+    }
+    public T get(int index) {
+        if(size<=index) {
+            return null;
+        }else{
+            IntNode p=sentinel.next;
+            int num=0;
+            while(num!=index){
+                p=p.next;
+                num++;
+            }
+            return p.first;
+
+        }
+    }
+
+
+ /*   public static void main (String[] args) {
+        LinkedListDeque <Integer>list = new LinkedListDeque();
+        list.addFirst(4);
+        list.addLast(3);
+       // System.out.println(list.get(1));
+        //list.printDeque();
+       // System.out.println(list.removeFirst());
+       // System.out.println(list.size());
+        System.out.println(list.getRecursive(1));
+    }*/
+}

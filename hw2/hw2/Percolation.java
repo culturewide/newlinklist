16fea24a127bfe8e7b2f8c5bfe1e8d6b94f4aae7
culturewide <eatfanbrother@gmail.com>
2025-05-26 21:02:51 +0800

    commit hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,94 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int[][] grid;
+    private int[] flatGrid;
+    private int num;//total open number
+    private  int N;
+    private WeightedQuickUnionUF uf ;
+
+    /**
+     *Represents the top and bottom of grid
+     * @param n creat a n by n grid,with all blocked
+     */
+
+    public Percolation(int n ){
+        N = n;
+        if(N <= 0){
+            throw new IndexOutOfBoundsException();
+        }
+        flatGrid = new int[N*N];
+        uf = new WeightedQuickUnionUF(N*N+2 );
+        grid = new int[N][N];
+        for(int i = 0; i <N; i++){
+            for(int j = 0; j < N; j++){
+                grid[i][j] = -2;
+                xyTo1D(i, j);
+            }
+        }
+        num = 0;
+        for(int i =0 ; i < N ; i++){
+            uf.union(i, N*N);
+            uf.union((N-1)*N+i, N*N+1);
+        }
+
+    }
+    // open the site (row, col) if it is not open already
+    //首先将row，col转换为一个数，
+    public void open(int row, int col){
+        int total = xyTo1D(row, col);
+        grid[row][col] = 1;
+        num++;
+        if(row-1 >=0 && grid[row-1][col] == 1){
+            uf.union(xyTo1D(row-1,col), xyTo1D(row,col));
+        }
+        if(row+1 < grid.length && grid[row+1][col] == 1){
+            uf.union(xyTo1D(row+1,col), xyTo1D(row,col));
+        }
+        if(col-1 >=0 && grid[row][col-1] == 1){
+            uf.union(xyTo1D(row,col-1), xyTo1D(row,col));
+        }
+        if(col+1 < grid[0].length && grid[row][col+1] == 1){
+            uf.union(xyTo1D(row,col+1), xyTo1D(row,col));
+        }
+    }
+    public boolean isOpen(int row, int col){
+        return grid[row][col] == 1;
+    }
+    public boolean isFull(int row, int col){
+        return uf.connected(xyTo1D(row, col), N*N);
+    }
+    public int numberOfOpenSites(){
+        return num;
+    }
+    public boolean percolates(){
+        if(uf.connected(N*N, N*N+1)){
+            return true;
+        }else{
+            return  false;
+        }
+    }
+    private int xyTo1D(int row, int col){
+        int rowCalculate = (row)*grid.length;
+        int colCalculate = col;
+        int total = rowCalculate + colCalculate;
+//        flatGrid[rowCalculate + colCalculate] = grid[row][col];
+        return total;
+    }
+    public static void main(String[] args) {
+        Percolation p = new Percolation(5);
+        p.open(2,2);
+        p.open(1,2);
+        p.open(0,2);
+        p.open(3,2);
+        if(p.percolates()){
+            System.out.println("yesohmygod");
+        }else System.out.println("no");
+        if(p.isFull(4,2)){
+            System.out.println("FUCK");
+        }
+
+    }
+
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,108 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
 
+public class PercolationStats {
+        private Percolation pl;
+        private int n;
+        private double o;
+//        private int t;
+        private int openSite;
+        private int trailTime;
+        private double u;
+        private double LowEndPoint;
+        private double HighEndPoint;
+        private PercolationFactory PF;
+        private double[] x;
+        public PercolationStats(int N, int T,PercolationFactory pf) {
+            PF = pf;
+            x = new double[T];
+            pl = pf.make(N);
+            openSite = 0;
+            n = N;
+            trailTime = 0;
+            int i = 0;
+            while(i < T){
+                randomOpen(pl);
+                i++;
+            }
+            u = mean();
+            o = stddev();
+            LowEndPoint = confidenceLow();
+            HighEndPoint = confidenceHigh();
+        }
+        private boolean checkIfPercolate(Percolation pl){
+            if(pl.percolates()){
+                return true;
+            }
+            return false;
+        }
+       /* private void randomOpen(Percolation pl) {
+               int  randomRow = StdRandom.uniform(0,n);
+               int  randomCol = StdRandom.uniform(0,n);
+               if(!pl.isOpen(randomRow,randomCol)){
+                       pl.open(randomRow,randomCol);
+                       openSite++;
+               }else{
+                       randomOpen(pl);
+               }
+               if(checkIfPercolate(pl)){
+                    x[trailTime] = (double) openSite/(n*n) ;
+                    trailTime++;
+                    resetPercolation();
+               }else{
+                   randomOpen(pl);
+               }
+
+        }*/
+       private void randomOpen(Percolation pl) {
+           while (!checkIfPercolate(pl)) {  // 直到系统渗透
+               int randomRow = StdRandom.uniform(0, n);
+               int randomCol = StdRandom.uniform(0, n);
+
+               if (!pl.isOpen(randomRow, randomCol)) {
+                   pl.open(randomRow, randomCol);
+                   openSite++;
+               }
+               // 如果格子已打开，继续循环，尝试下一个随机格子
+           }
+
+           // 记录结果并重置
+           x[trailTime] = (double) openSite / (n * n);
+           trailTime++;
+           resetPercolation();
+       }
+        private void resetPercolation() {
+            openSite = 0;
+            pl = PF.make(n);
+        }
+        //calculate the mean value of percolate
+        public double mean() {
+            double sum = 0;
+            double meanNum = 0;
+            for(int i=0;i < trailTime;i++){
+                sum += x[i];
+            }
+            meanNum = (double) sum / trailTime;
+            return meanNum;
+        }
+        public double stddev() {
+            double sum = 0;
+            for(int i=0;i < trailTime;i++){
+                sum += Math.pow((x[i]-u),2);
+            }
+            return Math.sqrt(sum / (trailTime - 1));
+        }
+        public double confidenceLow() {
+            return u-(1.96 * o) / Math.sqrt(trailTime);
+        }
+        public double confidenceHigh() {
+            return u+(1.96 * o) / Math.sqrt(trailTime);
+        }
+        public static void  main(String[] args) {
+            PercolationFactory pf = new PercolationFactory();
+            PercolationStats ps = new PercolationStats(400,30,pf);
+            System.out.println(ps.LowEndPoint);
+            System.out.println(ps.HighEndPoint);
+        }
 }
